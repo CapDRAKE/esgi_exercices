a51c3e698c23ea03f97e66583dd78758c386a39b
CapDRAKE <72074285+CapDRAKE@users.noreply.github.com>
2022-12-02 17:17:22 +0100

    commit 1

--- app/src/main/java/com/esgi/android/exercices/MainActivity.kt
+++ app/src/main/java/com/esgi/android/exercices/MainActivity.kt
@@ -8,6 +8,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         // TODO Changer cette ligne en fonction des layouts
+        //On utilise activity main pour le premier layout
         setContentView(R.layout.activity_main)
+
+        //Layout 2 => Vue détail. Commenter la ligne pour voir le layout 1
+        setContentView(R.layout.parking_receipt)
     }
+
+    //public void ParkingReceipt(){
+
+    //}
 }
